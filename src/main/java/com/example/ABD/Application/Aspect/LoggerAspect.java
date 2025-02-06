726f652782b63a1054e1f2f005dcbe66cc6cba1e
Abhishek <abhishekdound@gmail.com>
2025-02-06 12:31:33 +0530

    Logger aspect and performance monitoring aspect added

---
+++ src/main/java/com/example/ABD/Application/Aspect/LoggerAspect.java
@@ -0,0 +1,49 @@
+package com.example.ABD.Application.Aspect;
+
+import jakarta.persistence.Entity;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggerAspect {
+
+    private static final Logger logger= LoggerFactory.getLogger(LoggerAspect.class);
+
+    @Before("execution(* *.com.example.ABD.Application.Service.PersonService.findAll(..))")
+    public void before(JoinPoint jp){
+
+        logger.info(jp.getSignature().getName());
+
+
+    }
+
+    @After("execution(* *.com.example.ABD.Application.Service.PersonService.findAll(..))")
+    public void after(JoinPoint jp){
+
+        System.out.println(jp.getSignature().getName());
+
+
+    }
+
+    @AfterReturning("execution(* *.com.example.ABD.Application.Service.PersonService.findAll(..))")
+    public void afterReturning(JoinPoint jp){
+
+        System.out.println(jp.getSignature().getName());
+
+
+    }
+
+
+    @AfterThrowing("execution(* *.com.example.ABD.Application.Service.PersonService.findAll(..))")
+    public void afterThrowing(JoinPoint jp){
+
+        System.out.println(jp.getSignature().getName());
+
+
+    }
+
+}
---
+++ src/main/java/com/example/ABD/Application/Aspect/PerformanceMonitoringAspect.java
@@ -0,0 +1,33 @@
+package com.example.ABD.Application.Aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerformanceMonitoringAspect {
+
+    private static final Logger logger= LoggerFactory.getLogger(PerformanceMonitoringAspect.class);
+
+    @Around("execution(* *.com.example.ABD.Application.Service.PersonService.findAll(..))")
+    public Object performance(ProceedingJoinPoint pj) throws Throwable {
+
+        long start=System.currentTimeMillis();
+
+        pj.proceed();
+
+        long end=System.currentTimeMillis();
+
+        logger.info("Performance time is " + (start-end));
+
+        return pj;
+
+
+    }
+
+
+}
